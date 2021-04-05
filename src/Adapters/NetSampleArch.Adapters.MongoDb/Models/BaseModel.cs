3e64038911374e03b25b44a390a350453de324fe
italosantana <flaubert165@gmail.com>
2021-04-05 18:38:57 -0300

    datamodels command (sql) x query (mongo)

--- src/Adapters/NetSampleArch.Adapters.MongoDb/Models/BaseModel.cs
+++ src/Adapters/NetSampleArch.Adapters.MongoDb/Models/BaseModel.cs
@@ -1,9 +1,13 @@
 using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace NetSampleArch.Adapters.MongoDb.Models
 {
     public abstract class BaseModel
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public string CreatedBy { get; set; }
